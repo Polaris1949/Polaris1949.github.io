3e86e488af0d072c2ab564403140346464c09a56
Polaris1949 <Polaris1949@126.com>
2019-02-03 19:44:18 +0800

    New SWF Player

---
+++ tool/flash.js
@@ -0,0 +1,15 @@
+function play()
+{
+    var str = location.search;
+    var obj = '<object><embed id="swf" src="." width="100%" height="100%"></embed></object>';
+    str = str.substr(1);
+    obj = obj.replace('.', str);
+    document.getElementById('player').innerHTML += obj;
+}
+
+function jump()
+{
+    var link = document.getElementById('link').value;
+    while (link.length < 3) link = '0' + link;
+    location.href = 'player.html?' + link;
+}
